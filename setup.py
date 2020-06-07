057ee8f1e6b681b4478217952ef38e8f53cec535
Ilya Tyutin <emmarrgghh@gmail.com>
2020-06-07 10:47:50 +0300

    incremented version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="trava",
-    version="0.2.0",
+    version="0.2.1",
     description="Framework that helps to train models, compare them and track parameters&metrics along the way.",
     long_description=README,
     long_description_content_type="text/markdown",
